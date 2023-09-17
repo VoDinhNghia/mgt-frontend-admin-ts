e17dfb6fb140e04c4d8799f671df985dd675d778
VoDinhNghia <vodinhnghia85@gmail.com>
2023-09-17 16:10:53 +0700

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from './pages/login';
+import { routes } from './constants/constant';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={routes.login} element={<LoginPage />} />
+        <Route path='/' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
