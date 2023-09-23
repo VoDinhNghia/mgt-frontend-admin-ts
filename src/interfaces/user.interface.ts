3b96d1edabe348a86b10e67b343187f7dcab0aaa
VoDinhNghia <vodinhnghia85@gmail.com>
2023-09-23 20:32:47 +0700

    integrate api get list users and show data on table

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface IpayloadUpdateUser {
   email?: string;
   role?: string;
@@ -5,9 +6,44 @@ export interface IpayloadUpdateUser {
 }
 
 export interface IpayloadUpdateProfile {
-    firstName?: string;
-    lastName?: string;
-    middleName?: string;
-    gender?: string;
-    mobile?: string;
+  firstName?: string;
+  lastName?: string;
+  middleName?: string;
+  gender?: string;
+  mobile?: string;
+}
+
+export interface IuserTable {
+  name?: string;
+}
+
+export interface Iprofile {
+  lastName?: string;
+  firstName?: string;
+  middleName?: string;
+  code?: string;
+  gender?: string;
+  mobile?: string;
+}
+
+export interface IpropUserMgt {
+  dispatch?: any;
+  listUsers?: any;
+  totalUser?: any;
+}
+
+export interface IcolumnUserTable {
+  id?: number | any;
+  label?: string;
+  minWidth?: number;
+}
+
+export interface IrowUserTable {
+  index?: number;
+  name?: string;
+  email?: string;
+  code?: string;
+  role?: string;
+  status?: string;
+  award?: any;
 }
