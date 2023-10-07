85a911adcaf769b629dca0c1490e3c18658b976a
VoDinhNghia <vodinhnghia85@gmail.com>
2023-10-07 19:45:34 +0700

    show data on table money credit

--- src/utils/setting.util.ts
+++ src/utils/setting.util.ts
@@ -30,3 +30,59 @@ export const headerTableLearningRate = [
     minWidth: 102,
   },
 ];
+
+export const headerTableSubjectPass = [
+  {
+    id: "index",
+    label: "#",
+    minWidth: 120,
+  },
+  {
+    id: "name",
+    label: "Name",
+    minWidth: 120,
+  },
+  {
+    id: "type",
+    label: "Type",
+    minWidth: 120,
+  },
+  {
+    id: "condition",
+    label: "Condition",
+    minWidth: 120,
+  },
+  {
+    id: "actions",
+    label: "Actions",
+    minWidth: 102,
+  },
+];
+
+export const headerTableMoneyCredit = [
+  {
+    id: "index",
+    label: "#",
+    minWidth: 120,
+  },
+  {
+    id: "name",
+    label: "Name",
+    minWidth: 120,
+  },
+  {
+    id: "moneyPerCredit",
+    label: "Money Per Credit",
+    minWidth: 120,
+  },
+  {
+    id: "semester",
+    label: "Semester",
+    minWidth: 120,
+  },
+  {
+    id: "actions",
+    label: "Actions",
+    minWidth: 102,
+  },
+];
