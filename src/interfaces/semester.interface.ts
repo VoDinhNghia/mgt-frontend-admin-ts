c0e092e3611e82309354d3e07c811c83c1b918fc
VoDinhNghia <vodinhnghia85@gmail.com>
2023-10-10 19:19:00 +0700

    show data semester on table and pagination page

--- src/interfaces/semester.interface.ts
+++ src/interfaces/semester.interface.ts
@@ -1,5 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 export interface IcreateSemester {
   name?: string;
   year?: string;
+}
+
+export interface IpropSemester {
+  dispatch?: any;
+  listSemesters?: any;
+  totalSemester?: number;
+}
+
+export interface IrowTableSemester extends IcreateSemester {
+  _id?: string;
+  code?: string;
 }
