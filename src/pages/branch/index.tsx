5ef3e07f4de910ea4c6d5e08a5fd447f99e2dfa5
VoDinhNghia <vodinhnghia85@gmail.com>
2023-10-11 17:03:54 +0700

    integrate api and show infomation branch

--- src/pages/branch/index.tsx
+++ src/pages/branch/index.tsx
@@ -1,13 +1,55 @@
-import React from "react";
-import { validateAccessModule } from "../../utils/permission.util";
-import { moduleNames } from "../../constants/constant";
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import {
+  validateAccessModule,
+  validateAction,
+} from "../../utils/permission.util";
+import { moduleNames, permissonTypes } from "../../constants/constant";
 import MenuPage from "../commons/menu";
 import FooterPage from "../commons/footer";
 import ForbidenPage from "../commons/forbiden";
 import { Container } from "rsuite";
+import { connect } from "react-redux";
+import { IstateRedux } from "../../interfaces/common.interface";
+import {
+  IbranchCardItem,
+  IpropBranchPage,
+} from "../../interfaces/branch.interface";
+import { branchActions } from "../../store/actions";
+import AddAndSearchTable from "../commons/add-search-table";
+import { Card, Col, Row, Button } from "react-bootstrap";
+import { BsTrash, BsPencilSquare } from "react-icons/bs";
 
-const BranchMgtPage = () => {
+const BranchMgtPage = (props: IpropBranchPage) => {
+  const { listBranchs = [], dispatch } = props;
+  const [state, setState] = useState({
+    isShowModalAdd: true,
+  });
   const isAccess = validateAccessModule(moduleNames.BRANCH_MANAGEMENT);
+  const isPermissionAdd = validateAction(
+    permissonTypes.ADD,
+    moduleNames.BRANCH_MANAGEMENT
+  );
+  const fetchBranchs = () => {
+    dispatch({
+      type: branchActions.GET_LIST_BRANCH,
+    });
+  };
+
+  const onSearch = (searchKey: string) => {
+    dispatch({
+      type: branchActions.GET_LIST_BRANCH,
+      payload: {
+        searchKey,
+      },
+    });
+  };
+
+  useEffect(() => {
+    fetchBranchs();
+  }, []);
+
+  console.log("branchs", listBranchs, state);
 
   return (
     <div>
@@ -16,7 +58,57 @@ const BranchMgtPage = () => {
           <Container>
             <MenuPage />
             <Container className="p-3 fs-6">
-              <p>Branch management page</p>
+              <AddAndSearchTable
+                isDisableBtnAdd={!isPermissionAdd}
+                title="Add branch"
+                onSearch={(searchKey: string) => onSearch(searchKey)}
+                onShowAdd={() => setState({ ...state, isShowModalAdd: true })}
+              />
+              <Row>
+                {listBranchs?.map((branch: IbranchCardItem) => {
+                  const location = `${branch?.location?.address}, ${branch?.location?.ward?.name}, ${branch?.location?.district?.name}, ${branch?.location?.province?.name}, ${branch?.location?.country?.name}`;
+                  return (
+                    <Col xl={6} key={branch?._id}>
+                      <Card>
+                        <Card.Header className="bg-primary text-white">
+                          {branch?.title}
+                        </Card.Header>
+                        <Card.Body>
+                          <Card.Text>Name: {branch?.name}</Card.Text>
+                          <Card.Text>
+                            Website:{" "}
+                            <a href={branch?.website}>{branch?.website}</a>
+                          </Card.Text>
+                          <Card.Text>Location: {location}</Card.Text>
+                          <Card.Text>
+                            ContactInfo:{" "}
+                            <Button
+                              variant="outline-primary"
+                              className="border-0"
+                              size="sm"
+                            >
+                              View detail
+                            </Button>
+                          </Card.Text>
+                          <Card.Text>
+                            Description: {branch?.description}
+                          </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                          <span className="ActionGroupCardBranch">
+                            <Button variant="primary">
+                              <BsPencilSquare /> Update info
+                            </Button>{" "}
+                            <Button variant="danger">
+                              <BsTrash /> Delete
+                            </Button>
+                          </span>
+                        </Card.Footer>
+                      </Card>
+                    </Col>
+                  );
+                })}
+              </Row>
             </Container>
           </Container>
           <FooterPage />
@@ -28,4 +120,10 @@ const BranchMgtPage = () => {
   );
 };
 
-export default BranchMgtPage;
+const mapStateToProps = (state: IstateRedux) => {
+  return {
+    listBranchs: state.BranchReducer.listBranchs,
+  };
+};
+
+export default connect(mapStateToProps)(BranchMgtPage);
